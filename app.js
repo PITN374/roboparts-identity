328b1bbdad007fd3a84dc7e3fad65e86ccdacfbd
PITN374 <powerinthenumbers@pitn.ai>
2026-09-21 12:15:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve robots.txt
+// Power In The Numbers robots.txt
 app.get("/robots.txt", (req, res) => {
-  res.type("text/plain");
+  res.set("Content-Type", "text/plain; charset=utf-8");
   res.send(`User-agent: *
 Allow: /
 
@@ -13,22 +13,23 @@ Sitemap: https://powerinthenumbers.ai/sitemap.xml
 `);
 });
 
-// Serve sitemap.xml
+// Power In The Numbers sitemap.xml
 app.get("/sitemap.xml", (req, res) => {
-  res.type("application/xml");
+  res.set("Content-Type", "application/xml; charset=utf-8");
   res.send(`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://powerinthenumbers.ai/</loc>
   </url>
+  <url>
+    <loc>https://powerinthenumbers.ai/contact</loc>
+  </url>
+  <url>
+    <loc>https://powerinthenumbers.ai/partnership</loc>
+  </url>
 </urlset>`);
 });
 
-// Basic health check
-app.get("/", (req, res) => {
-  res.send("Power In The Numbers");
-});
-
 app.listen(PORT, () => {
-  console.log(\`Server running on port \${PORT}\`);
+  console.log("Power In The Numbers server running on port " + PORT);
 });
